85140d6946e9bc6231dfd1c3fa278c098eebcdd6
metzlr <reedmg15@gmail.com>
2020-10-30 02:43:59 -0700

    Created app loop with a command system. Added functionality for creating/reading entries.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import secrets
 import datetime
 import sys
 import fnmatch
+import getpass
 from cryptography.fernet import Fernet
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
@@ -53,8 +54,7 @@ def get_num_entries():
   return len(fnmatch.filter(os.listdir(ENTRIES_PATH), '*.entry'))
 
 
-def create_entry():
-  pwd = input("Enter your password: ")
+def create_entry(pwd):
 
   # Create entries directory if it doesn't exist
   if (not os.path.exists(ENTRIES_PATH)):
@@ -66,21 +66,68 @@ def create_entry():
   try:
     entry_msg = open(entry_path, "r").read()
   except:
-    print("ERROR unable to open file at path:", entry_path)
-    return
+    print("ERROR Unable to open file at path:", entry_path)
+    return False
 
   entry_number = get_num_entries() + 1
   now = datetime.datetime.now()
   entry_name = str(entry_number) + '___' + str(now.date()) + '_' + \
-      str(now.time()).replace(':', '-')
+      str(now.time()).replace(':', '-').replace('.', '-') + '.entry'
 
   encrypted = encrypt_message(entry_msg, pwd, DEFAULT_ITERATIONS)
   open(ENTRIES_PATH+entry_name, 'wb').write(encrypted)
+  return True
+
+
+def read_entry(pwd):
+  entries = list_entries()
+  i = int(input("Input the entry ID: "))
+  try:
+    entry_bytes = open(os.path.join(ENTRIES_PATH, entries[i]), "rb").read()
+  except:
+    print("ERROR Unable to open entry. Something might be wrong with the file")
+
+  msg = decrypt_message(entry_bytes, pwd)
+  print("\nMESSAGE:\n")
+  print(msg+"\n")
+
+
+def list_entries():
+  entries = [f for f in os.listdir(ENTRIES_PATH) if os.path.isfile(
+      os.path.join(ENTRIES_PATH, f)) and os.path.splitext(f)[-1].lower() == '.entry']
+  print("\nID\tNAME")
+  for i in range(len(entries)):
+    print("{num}\t{file_name}".format(num=i, file_name=entries[i]))
+  print()
+  return entries
 
 
 def main():
+  pwd = getpass.getpass(prompt='Enter password: ')
 
-  create_entry()
+  while (True):
+    cmd = input("journal: ")
+    if (cmd == "help"):
+      print("\nCommands:")
+      print("help\t\t List available commands")
+      print("create\t\t Create a new entry")
+      print("read\t\t Read an entry")
+      print("list\t\t Lists available entries")
+      print("quit\t\t Quit")
+      print()
+    elif (cmd == "write"):
+      success = create_entry(pwd)
+      if (success):
+        print("\nSuccessfully created new entry!\n")
+      else:
+        print("Failed to create entry")
+    elif (cmd == "read"):
+      read_entry(pwd)
+
+    elif (cmd == "list"):
+      list_entries()
+    elif (cmd == "quit"):
+      break
 
 
 if __name__ == "__main__":
