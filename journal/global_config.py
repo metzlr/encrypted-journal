713714b9d0dc17c7348fe4650fd5e2a2648a62a4
Reed <reedmg15@gmail.com>
2021-07-30 17:26:08 -0700

    Added config.ini file management. Entries path is now customizable

--- journal/global_config.py
+++ journal/global_config.py
@@ -1,12 +1,10 @@
-from pathlib import Path
 import os
 from dotenv import load_dotenv
+from pathlib import Path
+from configparser import ConfigParser
 
 load_dotenv()
 
-DEFAULT_ITERATIONS = 100000
-PWD_ITERATIONS = 500000
-PWD_MIN_LENGTH = 8
 
 PROD_DATA_PATH = Path.home().joinpath(".ejournal")
 DEV_DATA_PATH = Path("dev-data")
@@ -17,7 +15,24 @@ if os.getenv("DEV", "False").lower() == "true":
 else:
   DATA_PATH = PROD_DATA_PATH
 
-ENTRIES_PATH = DATA_PATH.joinpath("entries")
+CONFIG_PATH = DATA_PATH.joinpath("config.ini")
+config = ConfigParser()
+# Create config file and populate it with default values if it doesn't exist
+if not CONFIG_PATH.exists():
+  config["DEFAULT"] = {
+      "entries_path": DATA_PATH.joinpath("entries")
+  }
+  config["custom"] = {}
+  config.write(CONFIG_PATH.open("w"))
+else:
+  config.read(CONFIG_PATH)
+
+ENTRIES_PATH = Path(config.get("custom", "entries_path"))
+
 PASSWORD_KEY_PATH = DATA_PATH.joinpath("password.key")
 PASSWORD_KEY_PATH.suffix
 VERIFY_PASSWORD_MESSAGE = "This message will be encoded by the chosen password and used to verify that the correct password is being used."
+
+DEFAULT_ITERATIONS = 100000
+PWD_ITERATIONS = 500000
+PWD_MIN_LENGTH = 8
